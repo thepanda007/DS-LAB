60c09a0ca0c46bfeed8a772ee404e87daa8397a4
Sashanka Panda <213267846+thepanda007@users.noreply.github.com>
2026-04-02 21:58:47 +0530

    Experiment 9
    Opration on double linkedlist

---
+++ Double_linkedlist.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* prev;
+    struct Node* next;
+};
+
+struct Node* head = NULL;
+
+void insertAtBeginning(int x) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = x;
+    newNode->prev = NULL;
+    newNode->next = head;
+    if(head != NULL) head->prev = newNode;
+    head = newNode;
+}
+
+void deleteFirstNode() {
+    if(head == NULL) {
+        printf("List is empty\n");
+    } else {
+        struct Node* temp = head;
+        head = head->next;
+        if(head != NULL) head->prev = NULL;
+        printf("Deleted: %d\n", temp->data);
+        free(temp);
+    }
+}
+
+void displayList() {
+    if(head == NULL) {
+        printf("List is empty\n");
+    } else {
+        struct Node* temp = head;
+        printf("List elements: ");
+        while(temp != NULL) {
+            printf("%d ", temp->data);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int choice, num;
+    while(1) {
+        printf("1.Insert at beginning 2.Delete first node 3.Display 4.Exit\n");
+        scanf("%d", &choice);
+        switch(choice) {
+            case 1:
+                scanf("%d", &num);
+                insertAtBeginning(num);
+                break;
+            case 2:
+                deleteFirstNode();
+                break;
+            case 3:
+                displayList();
+                break;
+            case 4:
+                return 0;
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+}
